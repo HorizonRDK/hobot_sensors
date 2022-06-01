3303520237d63ab085bf9f9540874d0c5fc97a27
zhuo.wang <zhuo.wang@horizon.ai>
2022-06-01 16:29:57 +0800

    fix(mipi_cam gc4663): [ROBOT-45] Fix gc4663 init failed on x3pi; Adapt sensor name
    Summary: Fix the bug of gc4663 init failed on x3pi; Adapt sensor name lower and upper case letters
    
    Test Plan: selt test
    
    Reviewers: kao.zhu, wenli.wu
    
    Reviewed By: kao.zhu
    
    Subscribers: zhuo.wang
    
    Differential Revision: https://cr.hobot.cc/D181706

--- mipi_cam/src/mipi_cap.cpp
+++ mipi_cam/src/mipi_cap.cpp
@@ -195,9 +195,11 @@ int MipiDevice::init_param(void)
     m_oX3UsbCam.m_infos.m_vin_enable = 1;
     memset(sensor_name, 0, sizeof(sensor_name));
     snprintf(sensor_name, sizeof(sensor_name), "%s", m_oCamInfo.devName);
-    if (strcmp(sensor_name, "IMX415") == 0) {
+    if (strcmp(sensor_name, "IMX415") == 0 ||
+        strcmp(sensor_name, "imx415") == 0) {
         ret = mimx415_linear_vin_param_init(&m_oX3UsbCam.m_infos.m_vin_info);
-    } else if (strcmp(sensor_name, "F37") == 0) {
+    } else if (strcmp(sensor_name, "F37") == 0 ||
+               strcmp(sensor_name, "f37") == 0) {
         ret = mf37_linear_vin_param_init(&m_oX3UsbCam.m_infos.m_vin_info);
         int bus_num =
             m_oX3UsbCam.m_infos.m_vin_info.snsinfo.sensorInfo.bus_num;
@@ -216,8 +218,24 @@ int MipiDevice::init_param(void)
             }
           }
         }
-    } else if (strcmp(sensor_name, "GC4663") == 0) {
-        ret = mgc4663_linear_vin_param_init(&m_oX3UsbCam.m_infos.m_vin_info);
+    } else if (strcmp(sensor_name, "GC4663") == 0 ||
+               strcmp(sensor_name, "gc4663") == 0) {
+      ret = mgc4663_linear_vin_param_init(&m_oX3UsbCam.m_infos.m_vin_info);
+      int bus_num = m_oX3UsbCam.m_infos.m_vin_info.snsinfo.sensorInfo.bus_num;
+      std::string dev_i2c = "/dev/i2c-" + std::to_string(bus_num);
+      if (access(dev_i2c.data(), F_OK) != 0 && bus_num > 0) {
+        // 默认配置的i2c无效，自适应i2c号，实现对硬件平台的自适应。
+        for (int dev_id = bus_num - 1; dev_id >= 0; dev_id--) {
+          dev_i2c = "/dev/i2c-" + std::to_string(dev_id);
+          if (access(dev_i2c.data(), F_OK) == 0) {
+            m_oX3UsbCam.m_infos.m_vin_info.snsinfo.sensorInfo.bus_num = dev_id;
+            ROS_printf(
+                "Adapt bus_num for /dev/i2c- from %d to %d", bus_num,
+                m_oX3UsbCam.m_infos.m_vin_info.snsinfo.sensorInfo.bus_num);
+            break;
+          }
+        }
+      }
     } else {
         ROS_printf("[%s]->sensor name not found(%s).\n", __func__, sensor_name);
         m_oX3UsbCam.m_infos.m_vin_enable = 0;
