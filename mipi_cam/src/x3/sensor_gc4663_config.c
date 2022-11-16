b2f473ec8769198f50ea1850a5b997d33a163085
zhuo.wang <zhuo.wang@horizon.ai>
2022-11-16 11:55:56 +0800

    docs(mipi_cam): Update sensor config files licenses

--- mipi_cam/src/x3/sensor_gc4663_config.c
+++ mipi_cam/src/x3/sensor_gc4663_config.c
@@ -1,8 +1,17 @@
-/***************************************************************************
- * COPYRIGHT NOTICE
- * Copyright 2020 Horizon Robotics, Inc.
- * All rights reserved.
- ***************************************************************************/
+// Copyright (c) 2022，Horizon Robotics.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "sensor_gc4663_config.h"
 
 MIPI_SENSOR_INFO_S SENSOR_GC4663_30FPS_1440P_LINEAR_INFO = {
